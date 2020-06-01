24d68e56cdbee9fac83a262f6f658735bba96560
nlsjr <nelson.sjr.nsj@gmail.com>
2020-05-31 22:52:33 -0300

    fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyARjWHe6DRpv8e74urmOpxc4T7-uPMk4Dk",
-    authDomain: "eventos-3198f.firebaseapp.com",
-    databaseURL: "https://eventos-3198f.firebaseio.com",
-    projectId: "eventos-3198f",
-    storageBucket: "eventos-3198f.appspot.com",
-    messagingSenderId: "903481488019",
-    appId: "1:903481488019:web:02b1762b07c7be016c5366"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID
   };
 
   // Initialize Firebase
